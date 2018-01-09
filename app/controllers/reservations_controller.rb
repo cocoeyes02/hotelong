3868fa195ad00e0d850315d4f28a9ddd390f6297
ne260086 <ne260086@senshu-u.jp>
2018-01-09 21:48:55 +0900

    予約入力確認画面作成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,6 +14,18 @@ class ReservationsController < ApplicationController
   end
 
   def confirm
+    @reservation = Reservation.new(reservation_params)
+    room = Room.find_by(id: @reservation.room_id)
+    @room_number = room.room_number
+    plan = Plan.find_by(id: @reservation.plan_id)
+    @plan_name = plan.name
+
+    stay_date = @reservation.end_date - @reservation.start_date
+    if @reservation.plan_id.to_i == 1
+      @sum_price = Room.calculate_sum_price(@reservation.room_id, @reservation.guest_count) * stay_date
+    else
+      @sum_price = Plan.where(id: @reservation.plan_id.to_i).pluck(:price) * stay_date
+    end
   end
 
   def create
@@ -26,5 +38,9 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:room_id, :member_id, :guest_count, :plan_id, :start_date, :end_date)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,27 @@ class Room < ActiveRecord::Base
   has_many :plans, through: :plan_rooms
   has_many :plan_rooms
   belongs_to :reservation
+
+  def self.calculate_sum_price(room_id, guest_count)
+    # TODO: room_idとguest_countの値チェック
+    @room = Room.joins('JOIN class_rooms ON rooms.class_room_id = class_rooms.id')
+               .select('rooms.*, class_rooms.*')
+               .where('rooms.id = ?', room_id).first
+    if @room.nil?
+      # TODO: エラー処理
+    end
+    puts @room
+    sum_price = @room.person_price * guest_count
+    if guest_count > @room.person_price
+      0.upto(guest_count - @room.person_price) do
+        sum_price = sum_price * @room.surcharge_rate
+      end
+    end
+    if guest_count < @room.person_price
+      0.upto(@room.person_price - guest_count) do
+        sum_price = sum_price * @room.discount_rate
+      end
+    end
+    return sum_price
+  end
 end
